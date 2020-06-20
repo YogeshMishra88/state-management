61b1c35fbff73315a5f0360b5b97f2763a63bde9
Yogesh Mishra <yogesh.mishra88@gmail.com>
2020-06-20 08:49:37 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { PageNotFoundComponent} from './employees/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path:'',component:EmployeeListComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeAddComponent } from './employees/employee-add/employee-add.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { PageNotFoundComponent } from './employees/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeAddComponent,
+    EmployeeListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
